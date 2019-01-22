f7eaac842d6365a14c9a4862ed22b8999e41292b
sadin97 <sadinpita_97@windowslive.com>
2019-01-22 13:42:15 +0100

    selection system on voting component

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -10,7 +10,6 @@ export default {
   components: {
     GoBackArrow
   },
-  name: "",
   data: () => ({
   })
 }
--- src/components/Voting.vue
+++ src/components/Voting.vue
@@ -1,10 +1,27 @@
 <template>
   <div id="">
     <GoBackArrow />
-    {{this.$store.state.countryIdVoting}}
+    {{countryVotingId}}
+
+    <!-- <b-dropdown id="ddown1" text="Dropdown Button" class="m-md-2">
+      <b-dropdown-item v-for="(point, i) in points" :key="i">
+        {{point.score}}
+      </b-dropdown-item>
+    </b-dropdown> -->
+
+    <div class="pointList">
+      <div v-for="(point, i) in points" :key="i" @click="selectParent(i)">
+        <div v-bind:class="{ pointItem: !point.selected, pointItemSelected: point.selected }">{{point.score}}</div>
+        <div class="countryItem" v-for="(country, j) in countries" :key="j" v-if="point.selected">
+          <div v-if="country.ID !== countryVotingId">{{country.Name}}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import Service from '../services/index.js'
+const service = new Service()
 import GoBackArrow from '@/components/GoBackArrow'
 export default {
   name: "Voting",
@@ -12,9 +29,23 @@ export default {
     GoBackArrow
   },
   data: () => ({
-    countries: []
+    countries: [],
+    points: [
+      { score: 1, countrySelected: 'none', selected: false },
+      { score: 2, countrySelected: 'none', selected: false },
+      { score: 3, countrySelected: 'none', selected: false },
+      { score: 4, countrySelected: 'none', selected: false },
+      { score: 5, countrySelected: 'none', selected: false },
+      { score: 6, countrySelected: 'none', selected: false },
+      { score: 7, countrySelected: 'none', selected: false },
+      { score: 8, countrySelected: 'none', selected: false },
+      { score: 10, countrySelected: 'none', selected: false },
+      { score: 12, countrySelected: 'none', selected: false }
+    ],
+    countryVotingId: null
   }),
   created () {
+    this.countryVotingId = this.$store.state.countryIdVoting
     this.getAllCountries() // Ovdje dohvatam sve zemlje iz baze.
     if (this.$store.state.countryIdVoting === null) {
       this.$router.replace({name: 'home'})
@@ -32,10 +63,52 @@ export default {
       } catch (err) {
         console.log(err)
       }
+    },
+    selectParent (i) {
+      this.points.forEach(element => {
+        element.selected = false
+      })
+      this.points[i].selected = true
+      console.log('selektovao: ', i, ' i iskljucio sve ostale')
     }
   }
 }
 </script>
 <style lang="scss" scoped>
-
+.pointList {
+  padding-top: 5%;
+}
+.pointItem {
+  padding: 5px;
+  background-color: #ddd;
+  width: 40%;
+  margin: 0 auto;
+  border-bottom: 1px solid #eee;
+}
+.pointItem:hover {
+  cursor: pointer;
+  background-color: #bbb;
+  transition: 0.3s;
+}
+.pointItemSelected {
+  padding: 5px;
+  width: 40%;
+  margin: 0 auto;
+  border-bottom: 1px solid #eee;
+  background-color: #819ccc;
+}
+.pointItem:hover {
+  cursor: pointer;
+  background-color: #999;
+  transition: 0.3s;
+}
+.countryItem {
+  padding: 2px;
+  background-color: #b3c6e8;
+  width: 40%;
+  margin: 0 auto;
+  border-bottom: 1px solid #92a5c7;
+  border-left: 1px solid #92a5c7;
+  border-right: 1px solid #92a5c7;
+}
 </style>
